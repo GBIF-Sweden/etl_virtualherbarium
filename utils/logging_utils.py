a54a2409f7177d54af344216fb6f7903fc08a2b0
shahmanash <manash.shah@gmail.com>
2026-01-13 13:22:00 +0100

    Implement standard logging configuration

--- utils/logging_utils.py
+++ utils/logging_utils.py
@@ -1,4 +1,7 @@
 import logging
 
 def configure_logging():
-    pass
+    logging.basicConfig(level=logging.INFO,
+                        format='%(asctime)s - %(levelname)s - %(message)s',
+                        handlers=[logging.FileHandler("logs/app.log"),
+                                  logging.StreamHandler()])
