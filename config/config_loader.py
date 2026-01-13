136c18ffe5b96d065f740a1bd081575cf72e4af6
shahmanash <manash.shah@gmail.com>
2026-01-13 13:58:00 +0100

    Implement YAML configuration loader

--- config/config_loader.py
+++ config/config_loader.py
@@ -13,3 +13,15 @@ def load_json_config(config_path):
     except json.JSONDecodeError:
         logging.exception("Error: Failed to decode JSON from the configuration file.")
         raise
+
+
+def load_yaml_config(config_path):
+    try:
+        with open(config_path, 'r') as file:
+            return yaml.safe_load(file)
+    except FileNotFoundError:
+        logging.exception(f"Error: Configuration file not found at {config_path}.")
+        raise
+    except yaml.YAMLError:
+        logging.exception("Error: Failed to parse YAML from the configuration file.")
+        raise
