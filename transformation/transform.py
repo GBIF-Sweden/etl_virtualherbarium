6a4b3999088850cf4bcedc9be61d2d93b33464a0
shahmanash <manash.shah@gmail.com>
2026-01-14 11:38:00 +0100

    Implement whitespace cleaning and occurrence ID generation

--- transformation/transform.py
+++ transformation/transform.py
@@ -31,3 +31,49 @@ def apply_transformations(df, config, run_context=None):
     except Exception as e:
         logging.error(f"An error occurred during transformation: {e}")
         raise
+
+
+def clean_whitespace(df):
+    """
+    Clean unwanted whitespaces, tabs, and carriage returns from all string columns in the DataFrame.
+
+    Args:
+        df (pd.DataFrame): Input DataFrame.
+
+    Returns:
+        pd.DataFrame: Cleaned DataFrame.
+    """
+    try:
+        # Apply whitespace cleaning to each column
+        df_cleaned = df.map(
+            lambda x: ' '.join(str(x).split()).replace('\t', '').replace('\v', '') if isinstance(x, str) else x
+        )
+        logging.info(f"Whitespace cleaning transformation completed successfully.")
+        return df_cleaned
+
+    except Exception as e:
+        logging.exception(f"An error occurred during clean_whitespace: {e}")
+        raise
+
+
+def generate_occ_id_triplet(df):
+    """
+    Apply transformations creating occurrence IDs.
+
+    Args:
+        df (pd.DataFrame): Input DataFrame.
+
+    Returns:
+        pd.DataFrame: Transformed DataFrame.
+    """
+    try:
+        # Create occurrenceID by combining multiple fields
+        df['occurrenceID'] = df['institutionCode'] + ':' + df['collectionCode'] + ':' + df['catalogNumber'].astype(str)
+        # Reorder columns to move 'occurrenceID' to the first position
+        cols = ['occurrenceID'] + [col for col in df.columns if col != 'occurrenceID']
+        df = df[cols]
+    except Exception as e:
+        logging.exception(f"An unexpected error occurred in generate_occ_id_triplet: {e}")
+        raise
+
+    return df
