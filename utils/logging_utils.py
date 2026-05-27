670e7acf707365ff22487c3641ce2417b2412150
shahmanash <manash.shah@gmail.com>
2026-05-27 17:43:15 +0200

    create logs directory before file

--- utils/logging_utils.py
+++ utils/logging_utils.py
@@ -1,6 +1,8 @@
 import logging
+import os
 
 def configure_logging():
+    os.makedirs("logs", exist_ok=True)
     logging.basicConfig(level=logging.INFO,
                         format='%(asctime)s - %(levelname)s - %(message)s',
                         handlers=[logging.FileHandler("logs/app.log"),
